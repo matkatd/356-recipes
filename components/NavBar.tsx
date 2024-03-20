985219b67cfda8fae43a52d9a30fce24b6026f77
David T <matkatdavid@gmail.com>
2024-03-20 10:02:49 -0600

    Got basic layout and styles set up

---
+++ components/NavBar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "@/public/Logo.svg";
+import styles from "@/app/page.module.css";
+
+type NavBarProps = {};
+
+export const NavBar = ({}: NavBarProps) => (
+  <nav className="navBar">
+    <Image src={Logo} alt="Reciped Logo" />
+    <ul>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/recipes">Recipes</Link>
+      </li>
+      <li>
+        <Link href="/saved">Saved</Link>
+      </li>
+      <li>
+        <Link href="/about">About</Link>
+      </li>
+    </ul>
+  </nav>
+);
