6017df03fe5d64e961494b577623307ab595e6f9
David T <matkatdavid@gmail.com>
2024-03-20 10:32:01 -0600

    removed css

---
+++ app/recipe/[recipeId]/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+type RecipePageProps = {};
+
+export const RecipePage = ({}: RecipePageProps) => (
+  <>
+    <h1>React TS FC Component</h1>
+    <div>List</div>
+  </>
+);
